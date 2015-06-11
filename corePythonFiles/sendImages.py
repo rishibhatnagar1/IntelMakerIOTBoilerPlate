69252e82d9f4d077b8145f20daac297209da927a
rishibhatnagar1 <rishibhatnagar1@gmail.com>
2015-06-11 17:55:32 +0530

    servers added, readmes edited

---
+++ corePythonFiles/sendImages.py
@@ -0,0 +1,3 @@
+'''The code is written and maintained by Rishi Gaurav Bhatnagar  '''
+
+''' To send Images or sound files, you will need to do a mulitpart post, the following code will help you see how it works '''
